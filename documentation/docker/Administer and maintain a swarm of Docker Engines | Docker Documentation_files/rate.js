cd370b899cd980afa3744c4fdf0ebb8bb74623d4
cwc <icebowl1967@gmail.com>
2017-12-03 16:49:37 -0800

    aa

---
+++ documentation/docker/Administer and maintain a swarm of Docker Engines | Docker Documentation_files/rate.js
@@ -0,0 +1,6 @@
+PDRTJS_8453675.nero_up = 31;
+PDRTJS_8453675.nero_dn = 5;		
+PDRTJS_settings_8453675= { "type" : "nero" , "size" : "sml" , "star_color" : "hand" , "custom_star" : "" , "font_size" : "12px" , "font_line_height" : "20px" , "font_color" : "333333" , "font_align" : "center" , "font_position" : "top" , "font_family" : "verdana" , "font_bold" : "normal" , "font_italic" : "normal" , "text_vote" : "Vote" , "text_votes" : "Votes" , "text_rate_this" : "Rate this page:" , "text_1_star" : "Awful" , "text_2_star" : "Poor" , "text_3_star" : "Average" , "text_4_star" : "Good" , "text_5_star" : "Excellent" , "text_thank_you" : "Thank You" , "text_rate_up" : "Rate this page:" , "text_rate_down" : "Rate this page:" , "popup" : "off"  };
+PDRTJS_8453675.init();		
+PDRTJS_8453675.token='226569e8f3bc88c3213ad9b95003ded2';
+/*8453675,,engine/swarm/admin_guide.md,1148640305,31-5*/
