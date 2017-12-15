8c4f0e1997fd34c00ad110d57042c6d3f5dd5f58
cwc <icebowl1967@gmail.com>
2017-12-15 05:51:16 -0800

    aa

--- documentation/nodejs/server8080.js
+++ documentation/nodejs/server8080.js
@@ -1,15 +1,16 @@
-// add your server code in this file
-//home/cwc/nodeserver/server.js
 var http = require('http');
-fs = require('fs');
-var server = http.createServer(function(request,response){
-        if(request.url === '/'){
-                response.setHeader('Centent-Type','text/html');
-                //response.end('This is a simple test of a node server.');
-                fs.createReadStream('index.html').pipe(response);
-        }
-                console.log(request.url);
-        }); //end of http.createServer function
-server.listen(8080, function () {
-        console.log('port 8080 listen');
+ 
+var port = 8080;
+ 
+var s = http.createServer();
+s.on('request', function(request, response) {
+    response.writeHead(200);
+    console.log(request.method);
+    console.log(request.headers);
+    console.log(request.url);
+    response.write('42=20=*=00101010');
+    response.end();
 });
+ 
+s.listen(port);
+console.log('Browse to http://127.0.0.1:' + port);
