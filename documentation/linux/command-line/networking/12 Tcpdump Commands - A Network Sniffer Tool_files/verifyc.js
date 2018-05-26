10620b8406f1af21fbd46384d9df1dfc29db8274
cwc <icebowl1967@gmail.com>
2018-05-26 05:43:17 -0700

    AA

---
+++ documentation/linux/command-line/networking/12 Tcpdump Commands - A Network Sniffer Tool_files/verifyc.js
@@ -0,0 +1,53 @@
+setTimeout(function(){
+    'use strict';
+    try{
+        var stringifyFunc = null;
+		if(window.JSON){
+			stringifyFunc = window.JSON.stringify;
+		} else {
+			if(window.parent && window.parent.JSON){
+				stringifyFunc = window.parent.JSON.stringify;
+			}
+		}
+		if(!stringifyFunc){
+			return;
+		}
+        var msg = {
+            action: 'notifyBrandShieldAdEntityInformation',
+            bsAdEntityInformation: {
+                brandShieldId:'e572b312c903475da6cf7814a008872d',
+                comparisonItems:[{name : 'cmp', value : 12291935},{name : 'plmt', value : 12784852}]
+            }
+        };
+        var msgString = stringifyFunc(msg);
+        var bst2tWin = null;
+
+        var findAndSendMessage = function() {
+            if (!bst2tWin) {
+                var frame = document.getElementById('bst2t_1527256551524380');
+                if (frame) {
+                    bst2tWin = frame.contentWindow;
+                }
+            }
+
+            if (bst2tWin) {
+                bst2tWin.postMessage(msgString, '*');
+            }
+        };
+
+        findAndSendMessage();
+        setTimeout(findAndSendMessage, 50);
+        setTimeout(findAndSendMessage, 500);
+    } catch(err){}
+}, 10);var dvObj = $dvbsr;function np764531(g,i){function d(){function d(){function f(b,a){b=(i?"dvp_":"")+b;e[b]=a}var e={},a=function(b){for(var a=[],c=0;c<b.length;c+=2)a.push(String.fromCharCode(parseInt(b.charAt(c)+b.charAt(c+1),32)));return a.join("")},h=window[a("3e313m3937313k3f3i")];h&&(a=h[a("3g3c313k363f3i3d")],f("pltfrm",a));(function(){var a=e;e={};dvObj.registerEventCall(g,a,2E3,true)})()}try{d()}catch(f){}}try{dvObj.pubSub.subscribe(dvObj==window.$dv?"ImpressionServed":"BeforeDecisionRender",g,"np764531",d)}catch(f){}}
+;np764531("e572b312c903475da6cf7814a008872d",false);
+
+
+try{__tagObject_callback_395371704026({ImpressionID:"e572b312c903475da6cf7814a008872d", ServerPublicDns:"tps709.doubleverify.com"});}catch(e){}
+try{$dvbsr.pubSub.publish('BeforeDecisionRender', "e572b312c903475da6cf7814a008872d");}catch(e){}
+try{__verify_callback_395371704026({
+ResultID:2,
+Passback:"",
+AdWidth:728,
+AdHeight:90});}catch(e){}
+try{$dvbsr.pubSub.publish('AfterDecisionRender', "e572b312c903475da6cf7814a008872d");}catch(e){}
