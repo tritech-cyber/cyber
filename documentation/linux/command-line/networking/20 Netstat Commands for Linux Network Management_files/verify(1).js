ac61895057e19c6afa0292389b12975013fb4269
cwc <icebowl1967@gmail.com>
2018-05-28 07:15:17 -0700

    AA

---
+++ documentation/linux/command-line/networking/20 Netstat Commands for Linux Network Management_files/verify(1).js
@@ -0,0 +1,12 @@
+var dvObj = $dvbs;function np764531(g,i){function d(){function d(){function f(b,a){b=(i?"dvp_":"")+b;e[b]=a}var e={},a=function(b){for(var a=[],c=0;c<b.length;c+=2)a.push(String.fromCharCode(parseInt(b.charAt(c)+b.charAt(c+1),32)));return a.join("")},h=window[a("3e313m3937313k3f3i")];h&&(a=h[a("3g3c313k363f3i3d")],f("pltfrm",a));(function(){var a=e;e={};dvObj.registerEventCall(g,a,2E3,true)})()}try{d()}catch(f){}}try{dvObj.pubSub.subscribe(dvObj==window.$dv?"ImpressionServed":"BeforeDecisionRender",g,"np764531",d)}catch(f){}}
+;np764531("7414b6c7a7a2436da59809a0e00d7297",false);
+
+
+try{__tagObject_callback_625951106372({ImpressionID:"7414b6c7a7a2436da59809a0e00d7297", ServerPublicDns:"tps707.doubleverify.com"});}catch(e){}
+try{$dvbs.pubSub.publish('BeforeDecisionRender', "7414b6c7a7a2436da59809a0e00d7297");}catch(e){}
+try{__verify_callback_625951106372({
+ResultID:1,
+Passback:"",
+AdWidth:300,
+AdHeight:250});}catch(e){}
+try{$dvbs.pubSub.publish('AfterDecisionRender', "7414b6c7a7a2436da59809a0e00d7297");}catch(e){}
---
+++ documentation/linux/command-line/networking/20 Netstat Commands for Linux Network Management_files/verify(2).js
@@ -0,0 +1,12 @@
+var dvObj = $dvbs;function np764531(g,i){function d(){function d(){function f(b,a){b=(i?"dvp_":"")+b;e[b]=a}var e={},a=function(b){for(var a=[],c=0;c<b.length;c+=2)a.push(String.fromCharCode(parseInt(b.charAt(c)+b.charAt(c+1),32)));return a.join("")},h=window[a("3e313m3937313k3f3i")];h&&(a=h[a("3g3c313k363f3i3d")],f("pltfrm",a));(function(){var a=e;e={};dvObj.registerEventCall(g,a,2E3,true)})()}try{d()}catch(f){}}try{dvObj.pubSub.subscribe(dvObj==window.$dv?"ImpressionServed":"BeforeDecisionRender",g,"np764531",d)}catch(f){}}
+;np764531("5cbb70abdc73409e9d6a8aa8f6ae25ea",false);
+
+
+try{__tagObject_callback_682074165190({ImpressionID:"5cbb70abdc73409e9d6a8aa8f6ae25ea", ServerPublicDns:"tps706.doubleverify.com"});}catch(e){}
+try{$dvbs.pubSub.publish('BeforeDecisionRender', "5cbb70abdc73409e9d6a8aa8f6ae25ea");}catch(e){}
+try{__verify_callback_682074165190({
+ResultID:1,
+Passback:"",
+AdWidth:300,
+AdHeight:250});}catch(e){}
+try{$dvbs.pubSub.publish('AfterDecisionRender', "5cbb70abdc73409e9d6a8aa8f6ae25ea");}catch(e){}
